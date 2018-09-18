637bc291dd60504bb0367b82c9f1950b82118321
Resul134 <resulkurrr134@gmail.com>
2018-09-18 15:21:08 +0200

    Initial commit

---
+++ Survey/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("What do you do in your freem time?");
+            var sport = Console.ReadLine();
+
+
+            Console.WriteLine("Your name is:  " + (name));
+            Console.WriteLine("Your age is:  " + (age));
+            Console.WriteLine("You were born in:  " + (month));
+            Console.WriteLine("You enjoy   " +sport + "in your free time" );
+
+
+            if (month == "September") // Zodiac signs
+            {
+                Console.WriteLine("Your zodiac sign is Virgo");
+            }
+
+            if (month == "January")
+            {
+                Console.WriteLine("Your zodiac sign is Capricorn");
+            }
+        }
+    }
+}
